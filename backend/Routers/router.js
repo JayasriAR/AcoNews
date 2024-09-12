3892015a13ebaa8bff38cb5002e0d4b7e01d6b2c
Jayasri A R <109610628+JayasriAR@users.noreply.github.com>
2024-09-12 15:27:14 +0530

    Delete backend directory

--- backend/Routers/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../Controllers/controller');
-
-router.get('/top-headlines', controller.getTopHeadlines);
-router.get('/search', controller.searchNews);
-
-module.exports = router;
